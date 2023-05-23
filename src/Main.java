f686a3c69b87c9ac7ce405e0d08b52de30b12eab
Arthur Both <112208993+ArthurBoth@users.noreply.github.com>
2023-05-23 16:11:33 -0300

    Finalização da classe 'Main'
    A classe 'Main' agora está pronta para receber arquivos e iniciar os processos do programa.

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        String s = "12345 67890";
-        System.out.print("\'");
-        System.out.print(s.substring(0,s.indexOf(" ")));
-        System.out.println("\'");
-        System.out.print("\'");
-        System.out.print(s.substring(s.indexOf(" ")+1));
-        System.out.println("\'");
-        //new Navegacao();
+        new Navegacao().start("caso0.txt");
     }
 }
